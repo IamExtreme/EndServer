7e1868816862ba121b4f13517684b305fa861410
IamExtreme <luzsofiagn15@gmail.com>
2024-07-29 20:24:58 -0500

    Update AppRender.py

--- AppRender.py
+++ AppRender.py
@@ -12,7 +12,7 @@ class MQ135Data(BaseModel):
 # Lista para almacenar los datos temporales (simulando una base de datos en memoria)
 mq135_data_storage = []
 
-APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwDzXisbqosTwbnNyMPNZ6IpkzHNJjU1aMcscufSV4lDNGZ7QDJMaVC_MXO0a3Ubp7G/exec"
+APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxHlrp-TgQttqGAYhwx6CIho7sDFnodmedw7V7e1DXWkqyOJlrOPg2yWwVMvCfmjmq5VQ/exec"
 
 # Endpoint para recibir datos del sensor MQ135
 @app.post("/sensor/mq135/data/", response_model=MQ135Data)
